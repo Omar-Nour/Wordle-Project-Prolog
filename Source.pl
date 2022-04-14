a52cae784cd115ae6146fec667e39f7610c0930b
Omar Nour <52005044+Omar-Nour@users.noreply.github.com>
2022-04-14 14:17:00 +0100

    Update Source.pl

--- Source.pl
+++ Source.pl
@@ -1,11 +1,11 @@
 word(horse, animals).
 
-%Implemented by: Shamekh
+% Implemented by: Shamekh
 is_category(C):- 
   word(_, C).
 %
   
-%Implemented by: Omar
+% Implemented by: Omar
 pick_word(W, L, C):-
   word(W, C1),
   C1 = C,
@@ -13,7 +13,7 @@ pick_word(W, L, C):-
   L = L1.
 %
   
-%Implemented by: Ali
+% Implemented by: Ali
 correct_letters(L1,L2,CL):-
   CL = [H|T],
   correct_letters_helper(H, L1, [H|T]),
