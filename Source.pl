0e48563779f70ddbb6ec63f60d702ef9e44aadb1
Omar Nour <52005044+Omar-Nour@users.noreply.github.com>
2022-05-06 00:33:54 +0100

    Fully implementing the play predicate
    Assuming all other predicates are correct.

--- Source.pl
+++ Source.pl
@@ -60,32 +60,72 @@ init_game(W):-
 	write('Game started. You have 6 guesses.').
 
 choose_catgeory(List, C):-
-	write('Choose a category: '),
+	write('Choose a category: '), nl,
 	read(Input),
 	member(Input, List).
 	C = Input.
 
 choose_catgeory(List, C):-
-	write('Choose a category: '),
+	write('Choose a category: '), nl,
 	read(Input),
 	\+member(Input, List),
 	write('This category does not exist.'), nl,
 	choose_catgeory(List, C).
 	
 choose_length(L, Word_length):-
-	write('Choose a length: '),
+	write('Choose a length: '), nl,
 	read(Len),
 	number_string(N, Len),
 	member(N, L),
 	Word_length = N.
 
 choose_length(L, Word_length):-
-	write('Choose a length: '),
+	write('Choose a length: '), nl,
 	read(Len),
 	number_string(N, Len),
 	\+member(N, L),
 	write('There are no words of this length.'), nl,
 	choose_length(L, Word_length).
+	
+get_word(Length, W):-
+	write('Enter a word composed of '), write(Length), write(' letters:'), nl,
+	read(Input),
+	string_to_list(Input, Word),
+   	length(Word, Input_length),
+	Length \== Input_length,
+	write('Word is not composed of '), write(Length), write(' letters. Try again.'), nl,
+	write('Remaining Guesses are '), write(Length), nl, nl,
+	get_word(Length, W).
+	
+get_word(Length, W):-
+	write('Enter a word composed of '), write(Length), write(' letters:'), nl,
+	read(Input),
+	string_to_list(Input, Word),
+   	length(Word, Input_length),
+	Length == Input_length,
+	W = Word.
+	
+turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
+	Rem_guesses == 0,
+	write('You Lost').
+	
+turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
+	N_Rem_guesses is Rem_guesses - 1,
+	get_word(Length, Word),
+	W == Word,
+	write('You Won!').
+	
+turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
+	N_Rem_guesses is Rem_guesses - 1,
+	get_word(Length, Word),
+	W \== Word,
+	correct_letters(W, Word, Correct_letters),
+	correct_positions(W, Word, Correct_positions),
+	write('Correct letters are: '), write(Correct_letters), nl,
+	write('Correct letters in correct positions are: '), write(Correct_positions), nl,
+	write('Remaining Guesses are '), write(Length), nl, nl,
+	turn(N_Rem_guesses, W, Correct_letters, Correct_positions, Length).
+
 %
   
 % Implemented by: Ali
@@ -104,9 +144,14 @@ categories(L):-
   L = [H|T], is_category(H), categories(T).
 %
 
+play:-
+   init_game(W),
+   string_to_list(W, Word),
+   length(Word, Length),
+   turn(6, Word, Correct_letters, Correct_positions, Length).
+
 main:-
   write("Welcome to Pro-Wordle!"), nl,
   write("----------------------"), nl,
   build_kb,
-  init_game(W),
   play.
