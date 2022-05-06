80a00ff848a84fcac4bb0756b5060f6cf8255af4
Omar Nour <52005044+Omar-Nour@users.noreply.github.com>
2022-05-06 19:26:19 +0100

    Working Version

--- Source.pl
+++ Source.pl
@@ -1,16 +1,14 @@
 :- dynamic
         word/2.
 		
-%available_length(L):-
-%	word(X, _),
-%	atom_length(X, L).
+available_length(L):-
+	word(X, _),
+	atom_length(X, L).
 	
 available_length(L,C):-
 	word(X, C),
 	atom_length(X, L).	
 		
-
-% Implemented by: Shamekh
 is_category(C):- 
   word(_, C).
   
@@ -26,9 +24,7 @@ correct_positions([H1|T1], [H2|T2], PL):-
 		H1 \= H2,
 		correct_positions(T1, T2, PL)
 	).
-%
-  
-% Implemented by: Omar
+
 pick_word(W, L, C):-
     word(W, C),
 	atom_length(W, L).
@@ -52,18 +48,7 @@ build_kb:-
             assert(word(N, C)),
             build_kb
         )
-    ).
-    
-	%The available categories are: [animals,greetings,fruits, collections]
-	%Choose a category:
-	%|: animals.
-	%Choose a length:
-	%|: 9.
-	%There are no words of this length.
-	%Choose a length:
-	%|: 5.
-	%Game started. You have 6 guesses.
-	
+    ).	
 
 choose_catgeory(C):-
 	write('Choose a category: '), nl,
@@ -79,22 +64,6 @@ choose_catgeory(C):-
 		   choose_catgeory(C)
 		)
 	).	
-
-	
-%choose_length(Word_length):-
-%	write('Choose a length: '), nl, nl,
-%	read(Len),
-%	%number_string(N, [Len]),
-%	available_length(Len), !,
-%	Word_length = Len.
-
-%choose_length(L, Word_length):-
-%	write('Choose a length: '), nl,
-%	read(Len),
-%	%number_string(N, [Len]),
-%	\+available_length(Len), 
-%	write('There are no words of this length.'), nl,
-%	choose_length(Word_length).
 
 choose_length(Word_length,C):-
 	write('Choose a length: '), nl,
@@ -112,7 +81,6 @@ choose_length(Word_length,C):-
 		)
 	).
 
-
 init_game(W):-
  	write('The available categories are: '),
 	categories(List),
@@ -122,24 +90,6 @@ init_game(W):-
 	Guesses is Word_length + 1,
 	pick_random_word(W, Word_length, C),
 	write('Game started. You have '), write(Guesses), write(' guesses.'), nl.
-	
-%get_word(Length, W):-
-%	write('Enter a word composed of '), write(Length), write(' letters:'), nl,
-%	read(Input),
-%	string_to_list(Input, Word),
-%   length(Word, Input_length),
-%	Length \== Input_length,
-%	write('Word is not composed of '), write(Length), write(' letters. Try again.'), nl,
-%	write('Remaining Guesses are '), write(Length), nl, nl,
-%	get_word(Length, W).
-	
-%get_word(Length, W):-
-%	write('Enter a word composed of '), write(Length), write(' letters:'), nl,
-%	read(Input),
-%	string_to_list(Input, Word),
-%   length(Word, Input_length),
-%	Length == Input_length,
-%	W = Word.
 
 get_word(Length, W, Rem_guesses):-
 	write('Enter a word composed of '), write(Length), write(' letters:'), nl,
@@ -159,25 +109,6 @@ get_word(Length, W, Rem_guesses):-
 			get_word(Length, W, Rem_guesses) 
 		)
 	).
-	
-
-	
-%turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
-%	N_Rem_guesses is Rem_guesses - 1,
-%	get_word(Length, Word, Rem_guesses),
-%	W == Word,
-%	write('You Won!').
-	
-%turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
-%	N_Rem_guesses is Rem_guesses - 1,
-%	get_word(Length, Word, Rem_guesses),
-%	W \== Word,
-%	correct_letters(W, Word, Correct_letters),
-%	correct_positions(W, Word, Correct_positions),
-%	write('Correct letters are: '), write(Correct_letters), nl,
-%	write('Correct letters in correct positions are: '), write(Correct_positions), nl,
-%	write('Remaining Guesses are '), write(N_Rem_guesses), nl, nl,
-%	turn(N_Rem_guesses, W, Correct_letters, Correct_positions, Length).
 
 turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
 	Rem_guesses == 0,
@@ -206,27 +137,6 @@ turn(Rem_guesses, W, Correct_letters, Correct_positions, Length):-
 			turn(N_Rem_guesses, W, Correct_letters, Correct_positions, Length)
 		)
 	).
-
-%
-  
-% Implemented by: Ali
-
-%correct_letters(L1,[H|T],Cl):-
-%	correct_letters_h(L1,[H|T],[],Cl).
-
-%correct_letters_h(_, [], X, X).
-
-%correct_letters_h(L1,[H|T],Acc,Cl):-
-%	\+member(H,L1),
-%	correct_letters_h(L1,T,Acc,Cl).
-
-%correct_letters_h(L1,[H|T],Acc,Cl):-
-%	L1 is [H2|T2],
-%	member(H,L1),
-%	append(H,Acc),
-%	correct_letters_h(T2,T,Acc,Cl).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	
 correct_letters(L1, L2, Cl):-
 	L1 = [Input|T2],
@@ -247,11 +157,6 @@ correct_letters_h([Input|T2], L2, Cl, Acc):-
 		)
 	),
 	correct_letters_h(T2, L2, Cl, NewAcc).
-
-%correct_letters_helper(X, [], []).
-%correct_letters_helper(H,[H0|T0],CL):-
-%  (H = H0, CL is [H|T]);
-%  (H \= H0, correct_letters_helper(H, [T0], CL1)).
 
 categories(L):-
 	setof(C , X^word(X, C), L).
